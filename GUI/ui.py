c1fb6c9093f8f2ba84bd6f0a9b05778d6c7315b5
reppy4620 <ilikeniku@icloud.com>
2018-11-04 22:21:41 +0900

    fixed ui

---
+++ GUI/ui.py
@@ -0,0 +1,56 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.uix.image import Image
+from kivy.uix.label import Label
+from kivy.clock import Clock
+from kivy.properties import ObjectProperty
+from kivy.core.window import Window
+from kivy.config import Config
+from kivy.uix.popup import Popup
+from kivy.graphics import Color, Rectangle
+
+from Game.rule import Rule
+
+
+black = Image(source='image/black.png').texture
+white = Image(source='image/white.png').texture
+empty = Image(source='image/empty.png').texture
+
+
+class OthelloApp(App):
+    def build(self):
+        return Gui()
+
+
+class Tile(ButtonBehavior, Image):
+    def __init__(self, source):
+        super(Tile, self).__init__(source=source)
+
+    def on_press(self):
+        self.source = 'image/black.png'
+
+
+class Gui(BoxLayout):
+    grid = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        super(Gui, self).__init__(**kwargs)
+        self.tiles = list()
+        for y in range(Rule.BoardSize):
+            self.tiles.append(list())
+            for x in range(Rule.BoardSize):
+                if (x, y) in Rule.Black:
+                    im = Tile(source='image/black.png')
+                elif (x, y) in Rule.White:
+                    im = Tile(source='image/white.png')
+                else:
+                    im = Tile(source='image/empty.png')
+                self.tiles[y].append(im)
+                self.grid.add_widget(im)
+
+
+if __name__ == '__main__':
+    Window.size = (720, 720)
+    OthelloApp().run()
--- Game/rule.py
+++ Game/rule.py
@@ -5,3 +5,5 @@ class Rule:
     BoardSize = 8
     StartPlayer = Color.Black
     StartPosition = ((3, 3), (3, 4), (4, 4), (4, 3))
+    White = ((3, 3), (4, 4))
+    Black = ((3, 4), (4, 3))
